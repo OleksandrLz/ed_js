c121f7055b01b3f8cf594ab5f279f9fd3c9646a2
Oleksandr Lazarenko <lazsasha@ukr.net>
2017-02-22 20:25:08 +0200

    Add files via upload

---
+++ homework1/if_else_switch.js
@@ -0,0 +1,137 @@
+//1 задание
+
+//var a = 1;
+//if (a == 0){
+//	alert('верно');
+//} else {
+//	alert('неверно');
+//}
+
+//2 задание
+//
+//var a = 'test';
+//if (a == 'test'){
+//	alert('верно');
+//} else {
+//	alert('неверно');
+//}
+
+//3 задание
+
+//var test = true;
+//if (test){
+//	alert('верно');
+//} else {
+//	alert('неверно');
+//}
+//
+//var test = true ? alert('верно') : alert('неверно');
+
+//4 задание
+//var a = 2;
+//var b = 5;
+//var result = a + b;
+//if (result > 5){
+//	result = 5;
+//} else {
+//	result *= 10; 
+//}
+//alert(result);
+
+//5 задание
+//var a = 5;
+//if (a == 0 || a == 2 ){
+//	a /= 10;
+//} else {
+//	a += 7;
+//}
+//alert(a);
+
+//6 задание
+//var a = 1;
+//var b = 3;
+//if ( a <= 1 && b >= 3 ){
+//	alert(a + b);
+//} else {
+//	alert('неверно');
+//}
+
+//7 задание
+//var a = 2;
+//var b = 8;
+//if ((a > 2 && a < 11) || (b >= 6 && b < 14)){
+//	a += 2;
+//} else {
+//	a += 5;
+//}
+//alert(a);
+
+//8 задание
+//var num = 1;
+//switch (num) {
+//	case 1:
+//		result = 'зима';
+//	break;
+//	case 2:
+//		result = 'весна';
+//	break;
+//	case 3:
+//		result = 'лето';
+//	break;
+//	case 4:
+//		result = 'осень';
+//	break;
+//}
+//alert(num);
+
+//9 задание
+//var lang = 'ru';
+//var arr = [];
+//if (lang == 'ru'){
+//	arr = ['пн', 'вт'];
+//} else if (lang == 'eng'){
+//	arr = ['mon', 'tue'];
+//} else if (lang == 'de'){
+//	arr = ['mon', 'die'];
+//}
+//
+//var lang = 'ru';
+//switch (lang) {
+//	case 'ru':
+//		arr = ['пн', 'вт'];
+//	break;
+//	case 'eng':
+//		arr = ['mon', 'tue'];
+//	break;
+//	case 'de':
+//		arr = ['mon', 'die'];
+//	break;	
+//}
+//
+//var lang = 'ru';
+//var arr = [
+//	'ru' : ['пн', 'вт'],
+//	'eng' : ['mon', 'tue'],
+//	'de'  : ['mon', 'die']
+//];
+//alert(arr[lang]);
+
+//10 задание
+//var month = 1;
+//if (month >= 12 || month <= 2){
+//	var result = 'Зима';
+//} 
+//if (month >= 3 && month <= 5){
+//	result = 'Весна';
+//}
+//if (month >= 6 && month <= 8){
+//	result = 'Лето';
+//}
+//if (month >= 9 && month <= 11){
+//	result = 'Осень';
+//}
+//alert(result);
+	
+
+
+
