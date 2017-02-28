d9a7d7c0d7c69d71f017257e6e4de6e48166b2cd
Oleksandr Lazarenko <lazsasha@ukr.net>
2017-02-28 11:52:31 +0200

    Update loops-statements.js

--- lesson3/loops-statements.js
+++ lesson3/loops-statements.js
@@ -1,1 +1,37 @@
+//1 task
+for(var i = 0; i <= 15; i++){	
+	if(i%2 === 0){
+		document.write(i+' is even <br>');
+	} else {
+		document.write(i+' is odd <br>');
+	}
+}
 
+//2 task
+for(var i = 1; i <= 100; i++){	
+	if (i%3 === 0 && i%5 === 0){
+		document.write('FizzBuzz <br>');
+	} else if (i%3 === 0){
+		document.write('Fizz <br>');
+	} else if (i%5 === 0){
+		document.write('Buzz <br>');
+	}
+}
+
+//3 task
+var gcd = function(a, b) {
+    if ( ! b) {
+        return a;
+    }
+    return gcd(b, a % b);
+};
+alert(gcd(15,10));
+
+//4 task
+var sum = 0;
+for(var i = 1; i <= 1000; i++){	
+	if (i%3 === 0 && i%5 === 0){
+		sum = sum + i;
+	} 
+}
+document.write(sum);
