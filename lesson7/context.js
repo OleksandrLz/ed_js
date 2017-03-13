288b47a0bbf06b3d5ea8ede566f9398c0fb6cff9
Oleksandr Lazarenko <lazsasha@ukr.net>
2017-03-13 19:56:59 +0200

    Add files via upload

---
+++ lesson7/context.js
@@ -0,0 +1,37 @@
+Опеределить на какой контекст будет ссылатся this в результате выполнений следующих примеров
+
+function isContextEqualTo(contextLink) {
+   console.log(contextLink === this);
+}
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+
+    this.getName = function () {
+       isContextEqualTo(window); // в контексте window
+       isContextEqualTo(user); // в контексте window
+       return this.name;
+    };
+}
+
+Person.prototype.getAge = function () {
+    isContextEqualTo(window); // в контексте window
+    return this.age;
+};
+
+var user = new Person("erer", 45);
+
+var getAge = Person.prototype.getAge;
+var getName = user.getName;
+
+console.log(getAge()); //  в контексте window
+console.log(getName()); //  в контексте window
+console.log(user.getName()); //  в контексте user
+
+Person.prototype.getAge = function() {
+    isContextEqualTo(user); //  в контексте window
+    return this.name;
+}
+
+console.log(user.getAge()); //  в контексте user
